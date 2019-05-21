48046a6a0192c18b9ed46d9e3538e8b247648a70
Syed Yousha <yousha.m.syed@gmail.com>
2019-05-20 19:10:44 -0500

    1st Commit

--- CharacterInput.py
+++ CharacterInput.py
@@ -10,3 +10,5 @@ year = str((2019 - age) + 100) #Declared string here because you're
 
 print('Hello,', name,', you are', age, 'years old! In the year', year,
 'you will be 100 years old!' )
+
+print('Hello')
